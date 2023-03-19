df4992139a2d34181589d5b671118781f0c7f709
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 21:39:01 +0530

    Add files via upload

---
+++ identifydatatypes.c
@@ -0,0 +1,13 @@
+//Identifying data types 
+#include <stdio.h>
+int main(){
+ float x=3.69;
+ int y=3;
+ char z='A';
+ double a=3.1498527;
+ printf("%f\n",x);
+ printf("%d\n",y);
+ printf("%c\n",z);
+ printf("%lf\n",a);
+ return 0;
+}

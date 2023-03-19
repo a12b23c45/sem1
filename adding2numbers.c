f6b00da86a57f781b5f55cc15d19ac16e7711f60
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 21:35:18 +0530

    Add files via upload

---
+++ adding2numbers.c
@@ -0,0 +1,23 @@
+ //COMPUTER LAB
+//Adding 2 numbers without scanf
+#include <stdio.h>
+int main()
+{
+ int a,b,sum;
+ a=12;b=24;
+ sum=a+b;
+ printf("The sum of two numbers is %d",sum);
+}
+ 
+ 
+//ADD TWO NUMBERS USING INTEGERS 
+#include <stdio.h>
+int main()
+{
+ int num1;
+ float num2,res;
+ scanf("%d%f",&num1,&num2);
+ res=num1+num2;
+ printf("addition of %d and %f is %f",num1,num2,res);
+}
+ 

885d17afee3a58c94920088a8459a86b89e976b7
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 21:47:01 +0530

    Add files via upload

---
+++ predecrement.c
@@ -0,0 +1,7 @@
+PRE DECREMNET 
+int main()
+{
+    int m=4,y;                        output=m,y=3
+    y=--m; //5(modified)          (m++ for post decrement output m=3y=4)                      
+    printf("m=%d y=%d",m,y);
+}

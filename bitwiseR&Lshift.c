c071bb65d7f37882c96911985e057d15cc3e2ff7
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 21:49:22 +0530

    Add files via upload

---
+++ bitwiseR&Lshift.c
@@ -0,0 +1,10 @@
+//BIT WISE RIGHT SHIT(>>) BIT WISE LEFT SHIFT(<<)
+int main()
+{
+    int a=8,b=3,x,y;
+    x=a<<b; // left shift
+    y=a>>b;//right shift
+    
+    printf("%d\n",x);
+     printf("%d\n",y);
+}

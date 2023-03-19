93dec2eb27c26de0e3ad6face5bd49b37ea79c92
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 21:45:54 +0530

    Add files via upload

---
+++ postincrement.c
@@ -0,0 +1,9 @@
+//POST INCREMENT
+#include <stdio.h>
+ 
+int main()                                                          output= m=5,y=4
+{
+    int m=4,y;
+    y=m++; //5(modified)
+    printf("m=%d y=%d",m,y);
+}

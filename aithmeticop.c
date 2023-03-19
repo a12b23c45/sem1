18e5b48e4b89035f625da0b2fe4bbf21fb68605b
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 22:17:57 +0530

    Add files via upload

---
+++ aithmeticop.c
@@ -0,0 +1,25 @@
+//arithmetic operators sum diff
+
+#include<stdio.h> 
+
+int main () 
+
+{ 
+
+    int a,b,sum,difference,product,quotient,remainder; 
+
+    scanf("%d%d",&a,&b); 
+
+    sum=a+b; 
+
+    difference=a-b; 
+
+    product=a*b; 
+
+    quotient=a/b; 
+
+    remainder=a%b; 
+
+    printf("%d %d %d %d %d",sum,difference,product,quotient,remainder); 
+
+} 

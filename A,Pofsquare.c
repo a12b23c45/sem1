c47b049721061b99d7ddb610b7258fb5490f2eac
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-19 21:41:29 +0530

    Add files via upload

---
+++ A,Pofsquare.c
@@ -0,0 +1,11 @@
+//TO FIND AREA AND PERIMETER OF A SQUARE
+#include <stdio.h>
+int main()
+{
+ int side,area,peri;
+ scanf("%d",&side);
+ area=side*side;
+ peri=4*side;
+ printf("%d %d",area,peri);
+ 
+}

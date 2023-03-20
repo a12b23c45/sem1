3f4648e1ac57499c39f91f3ef852195ed88ca708
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-20 14:30:54 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,15 @@
+//creating array 
+#include <stdio.h>
+ 
+int main()
+{
+    int marks[5];
+    marks[0]=10;
+    marks[1]=12;
+    marks[2]=34;
+    marks[3]=45;
+    marks[4]=5;
+    marks[5]=40;
+    for (int i=0; i<=5; i++){
+    printf("%d ", marks[i]);}
+}

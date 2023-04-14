d9316233f6f1586d71e98a61eea28c8eb0cb27d5
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-04-14 17:45:01 +0530

    Add files via upload

---
+++ gen-matrix.c
@@ -0,0 +1,19 @@
+// creating a 4 by 2 matric using array 
+#include <stdio.h>
+
+int main() {
+    int i;
+    int a[4];
+    
+    for(int i=0;i<=4;i++){
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<=4;i++){
+        printf(" %d",a[2]);
+    }
+    printf("\n");
+    for(i=0;i<=4;i++){
+        printf(" %d",a[4]);
+    }
+}
